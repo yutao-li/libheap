58b90a5423d4837ba95082158699b00447aadd37
master_lee <870070392@qq.com>
2018-12-31 22:54:17 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='libheap',
+      version='0.1',
+      description='a general heap library',
+      url='https://github.com/yutao-li/libheap,
+      author='masterlee',
+      author_email='',
+      license='MIT',
+      packages=['libheap'],
+      install_requires=[])
