87eaf8c57724e4d1d20aeee87586ecd3dd40f22b
master_lee <yutao@ucsd.edu>
2019-05-27 14:40:43 -0700

    published version

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ setup(name='libheap',
       version='0.3',
       description='a general heap library',
       url='https://github.com/yutao-li/libheap',
+      download_url='https://github.com/yutao-li/libheap/archive/0.3.tar.gz',
       author='masterlee',
-      author_email='',
+      author_email='870070392@qq.com',
       packages=['libheap'],
-      license='MIT')
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3'])
